a1bb6dc1cca64aa2a06985e392a1ff6df4a230b9
Zoe Bai <s.xue.bai@gmail.com>
2019-02-13 02:20:32 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -45,7 +45,6 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  age = []
   data.each do |each_season, info| 
     if each_season.to_s == season
      info.each do |element|
@@ -54,6 +53,7 @@ def get_average_age_for_season(data, season)
    end 
   end
    
+  age = 0
   age.collect do |num|
     age += num.to_i
   end
