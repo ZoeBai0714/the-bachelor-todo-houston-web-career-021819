78409e2a0c9571ae01c03235e30c6b3aad533e3b
Zoe Bai <s.xue.bai@gmail.com>
2019-02-13 02:11:38 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -35,7 +35,13 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |each_season, info| 
+     info.each do |element|
+       if element["hometown"] == hometown
+        return element["occupation"]
+       end
+     end 
+   end
 end
 
 def get_average_age_for_season(data, season)
