99d8f0e79d9b19ea0d6aedcd0ea5b15eb1bdef45
Zoe Bai <s.xue.bai@gmail.com>
2019-02-13 01:59:32 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -16,11 +16,11 @@ def get_contestant_name(data, occupation)
      info.each do |element|
        if element["occupation"] == occupation
         return element["name"]
-      
     end
    end 
   end
 end
+
 
 def count_contestants_by_hometown(data, hometown)
   # code here
